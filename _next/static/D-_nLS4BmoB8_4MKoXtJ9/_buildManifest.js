7714577a53e14c332f3b2561d01f4aec72ee00c6
ilhamazhar <ilhamazhar@users.noreply.github.com>
2022-08-31 12:54:07 +0000

    deploy: af2e67d1bce0a375d4aa81e1d007b3572dbee6a4

---
+++ _next/static/D-_nLS4BmoB8_4MKoXtJ9/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fd5107cff0c5231e.css","static/chunks/pages/index-17759284458b8f9d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
